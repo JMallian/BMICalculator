9dd435c6a54da4435531dcc93c8c9700f9f0822a
Jessica Mallian <jlmallian@gmail.com>
2018-08-13 06:58:36 -0500

    initial commit

---
+++ BMICalculator/BodyCalculations.swift
@@ -0,0 +1,14 @@
+//
+//  BodyCalculations.swift
+//  BMICalculator
+//
+//  Created by Jessica Mallian on 8/13/18.
+//  Copyright © 2018 Jessica Mallian. All rights reserved.
+//
+
+import Foundation
+
+class BodyCalculations {
+    //TODO: probably a bunch of static methods, write tests!
+    
+}
